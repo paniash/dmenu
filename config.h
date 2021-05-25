88215518a67c91728e88a51a5dcc0a6bbfc31a3e
Ashish Panigrahi <ashish.panigrahi@protonmail.com>
2021-05-25 19:48:26 +0530

    Updated colors and fonts

--- config.h
+++ config.h
@@ -5,17 +5,20 @@ static int topbar = 1;                      /* -b  option; if 0, dmenu appears a
 static int fuzzy = 0;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"Iosevka:size=12"
+	"Terminus (TTF):size=15"
 };
 
 static const char col_cyan1[]   = "#005577";
 static const char col_cyan2[]   = "#007777";
+static const char col_gray1[]   = "#222222";
+static const char col_gray3[]   = "#bbbbbb";
+static const char col_gray4[]   = "#eeeeee";
 
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#050000", col_cyan2},
+	[SchemeNorm] = { col_gray3, col_gray1 },
+	[SchemeSel] = { col_gray4, col_cyan1 },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
